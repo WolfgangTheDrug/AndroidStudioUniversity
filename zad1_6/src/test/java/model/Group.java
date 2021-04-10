81ab168bc862ca99f48dd8a671b6d1a428a59f7e
Wolfgang <bardak_widz@wp.pl>
2021-04-10 19:57:40 +0200

    added basic functionalities to Person and Group classes

--- zad1_6/src/test/java/model/Group.java
+++ zad1_6/src/test/java/model/Group.java
@@ -1,4 +1,30 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Group {
+    private String groupName;
+    private List<Person> children;
+
+    public Group(String groupName, List<Person> children) {
+        this.groupName = groupName;
+        this.children = new ArrayList<>(children);
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public int size() {
+        return children.size();
+    }
+
+    public void addChild(Person person) {
+        children.add(person);
+    }
+
+    public Person getChild(int position) {
+        return children.get(position);
+    }
 }
